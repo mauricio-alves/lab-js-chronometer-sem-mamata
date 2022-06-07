827a2ec641fc0e1ed5ea5f2854f8c84b08e9c510
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-06-07 18:42:01 -0300

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
+    // setInterval(() => printTimeCallback, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime % 60 === 0) {
+      return Math.floor(this.currentTime / 60);
+    } else if (this.currentTime > 60) {
+      return this.currentTime - 60;
+    } else {
+      return this.currentTime;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoNumbers = value.toString();
+    if (value === 0) {
+      twoNumbers = '00';
+    } else if (value <= 9) {
+      twoNumbers = '0' + twoNumbers;
+    } else {
+      twoNumbers;
+    }
+    return twoNumbers;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    // const result = `${this.computeTwoDigitNumber(
+    //   this.getMinutes
+    // )}:${this.computeTwoDigitNumber(this.getSeconds)}`;
+    // return result
   }
 }
 
